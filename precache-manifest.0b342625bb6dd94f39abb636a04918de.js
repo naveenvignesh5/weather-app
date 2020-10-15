99ad28eab1bd1b4271cf8d5052b3d540a536cffb
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-10-15 13:15:14 +0530

    Updates

---
+++ precache-manifest.0b342625bb6dd94f39abb636a04918de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ce2e204e72837e22d0e8235d4911e2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "504ff2b7ee18badce36b",
+    "url": "/weather-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9709501043174d3ad56a",
+    "url": "/weather-app/static/js/2.e5e6ea14.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/weather-app/static/js/2.e5e6ea14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504ff2b7ee18badce36b",
+    "url": "/weather-app/static/js/main.43aad6ab.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
