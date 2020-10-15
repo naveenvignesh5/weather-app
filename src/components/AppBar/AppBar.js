cb6851d738c74997f2b404b36a1bbe2baeac6a21
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-10-15 11:38:13 +0530

    search bar, forcast section ui updates

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,15 +1,57 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import MuiAppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
-  menuButton: {
-    marginRight: theme.spacing(2),
+  grow: {
+    flexGrow: 1,
   },
   title: {
-    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
   },
 }));
 
@@ -17,12 +59,28 @@ export function AppBar() {
   const classes = useStyles();
 
   return (
-    <MuiAppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          Weather App
-        </Typography>
-      </Toolbar>
-    </MuiAppBar>
+    <div className={classes.grow}>
+      <MuiAppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Weather App
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search Location"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <div className={classes.grow} />
+        </Toolbar>
+      </MuiAppBar>
+    </div>
   );
 }
--- src/components/WeatherCard/WeatherForcast/WeatherForcast.js
+++ src/components/WeatherCard/WeatherForcast/WeatherForcast.js
@@ -2,10 +2,19 @@ import React from 'react';
 import get from 'lodash/get';
 import moment from 'moment';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+  desc: {
+    textTransform: 'capitalize',
+  },
+});
+
 export function WeatherForcast({ forcastData }) {
+  const classes = useStyles();
+
   return (
     <Grid container>
       {forcastData.map(function (data, index) {
@@ -25,10 +34,12 @@ export function WeatherForcast({ forcastData }) {
           >
             <Typography variant="h5">{moment(dt).format('dddd')}</Typography>
             <img
-              alt={icon}
+              alt={desc}
               src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
             />
-            <Typography color="textSecondary">{desc}</Typography>
+            <Typography classes={{ root: classes.desc }} color="textSecondary">
+              {desc}
+            </Typography>
           </Grid>
         );
       })}
